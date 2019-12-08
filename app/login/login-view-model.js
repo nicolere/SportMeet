e1b8003cfa8d7ba6061860abe4768dd698e2e02a
Nicolas <nicolas.gauss.1@orange.fr>
2019-12-08 21:57:23 +0100

    fix stuff

--- app/login/login-view-model.js
+++ app/login/login-view-model.js
@@ -7,22 +7,21 @@ const CheckBox = require("@nstudio/nativescript-checkbox").CheckBox;
 const userService = require("~/services/user-service");
 
 function LoginViewModel() {
-
     const viewModel = observableModule.fromObject({
         email: "user@nativescript.org",
         password: "password",
         confirmPassword: "",
-        surname:"DELATTRE Edouard",
-        age:22,
-        name:"",
+        surname: "DELATTRE Edouard",
+        age: 22,
+        name: "",
         title: "SportMeet",
-        date:"",
-        items:{},
-        Genre : ["Homme","Femme"],
+        date: "",
+        items: {},
+        Genre: ["Homme", "Femme"],
         isLoggingIn: true,
         isMyProfilModif: true,
         processing: false,
-        titreView:"Paramètres",
+        titreView: "Paramètres",
 
         //Lorsque l'on passe dans la phase d'inscription et vice versa
         toggleForm() {
@@ -33,7 +32,9 @@ function LoginViewModel() {
         //Lors de l'appuie sur le bouton de connexion
         submit() {
             if (this.email.trim() === "" || this.password.trim() === "") {
-                alert("Veuillez rentrer une adresse mail et un mot de passe valide.");
+                alert(
+                    "Veuillez rentrer une adresse mail et un mot de passe valide."
+                );
                 return;
             }
 
@@ -46,20 +47,23 @@ function LoginViewModel() {
         },
         login() {
             console.log("la co");
-            userService.login({
-                email: this.email,
-                password: this.password
-            }).then(() => {
-                this.set("processing", false);
-                topmost().navigate({
-                    moduleName: "/home/home-page",
-                    clearHistory: true
+            userService
+                .login({
+                    email: this.email,
+                    password: this.password
+                })
+                .then(() => {
+                    this.set("processing", false);
+                    topmost().navigate({
+                        moduleName: "/home/home-page",
+                        clearHistory: true
+                    });
+                })
+                .catch(e => {
+                    this.set("processing", false);
+                    console.log(e);
+                    alert("Impossible de retrouver votre compte.");
                 });
-            }).catch((e) => {
-                this.set("processing", false);
-                console.log(e);
-                alert("Impossible de retrouver votre compte.");
-            });
         },
 
         //Lors de la création du compte on vérifie le mot de passe et si tout est bon on accepte la création
@@ -69,77 +73,96 @@ function LoginViewModel() {
                 return;
             }
             //On enregistre le mot de passe et l'adresse mail
-            userService.register({
-                email: this.email,
-                password: this.password
-            }).then(() => {
-                this.set("processing", false);
-                alert("Votre compte vient d'être créé, vous pouvez vos connecter.");
-                this.isLoggingIn = true;
-            })
+            userService
+                .register({
+                    email: this.email,
+                    password: this.password
+                })
+                .then(() => {
+                    this.set("processing", false);
+                    alert(
+                        "Votre compte vient d'être créé, vous pouvez vos connecter."
+                    );
+                    this.isLoggingIn = true;
+                })
                 .catch(() => {
                     this.set("processing", false);
-                    alert("Unfortunately we were unable to create your account.");
+                    alert(
+                        "Unfortunately we were unable to create your account."
+                    );
                 });
         },
         forgotPassword() {
-            dialogsModule.prompt({
-                title: "Forgot Password",
-                message: "Enter the email address you used to register for APP NAME to reset your password.",
-                inputType: "email",
-                defaultText: "",
-                okButtonText: "Ok",
-                cancelButtonText: "Cancel"
-            }).then((data) => {
-                if (data.result) {
-                    userService.resetPassword(data.text.trim())
-                        .then(() => {
-                            alert("Your password was successfully reset. Please check your email for instructions on choosing a new password.");
-                        }).catch(() => {
-                            alert("Unfortunately, an error occurred resetting your password.");
-                        });
-                }
-            });
+            dialogsModule
+                .prompt({
+                    title: "Forgot Password",
+                    message:
+                        "Enter the email address you used to register for APP NAME to reset your password.",
+                    inputType: "email",
+                    defaultText: "",
+                    okButtonText: "Ok",
+                    cancelButtonText: "Cancel"
+                })
+                .then(data => {
+                    if (data.result) {
+                        userService
+                            .resetPassword(data.text.trim())
+                            .then(() => {
+                                alert(
+                                    "Your password was successfully reset. Please check your email for instructions on choosing a new password."
+                                );
+                            })
+                            .catch(() => {
+                                alert(
+                                    "Unfortunately, an error occurred resetting your password."
+                                );
+                            });
+                    }
+                });
         },
 
-        getToProfil(){
+        getToProfil() {
             topmost().navigate({
                 moduleName: "profil/profil-page",
                 clearHistory: true
             });
         },
 
-        goToEvents(){
+        goToEvents() {
             topmost().navigate({
                 moduleName: "events/events-page",
                 clearHistory: true
             });
         },
 
-        deconnexion()
-        {
+        deconnexion() {
             topmost().navigate({
                 moduleName: "login/login-page",
                 clearHistory: true
             });
         },
 
-        goToMenu()
-        {
+        goToMenu() {
             topmost().navigate({
                 moduleName: "menu/menu-page",
                 clearHistory: true
             });
         },
 
-        goToFriends()
-        {
+        goToFriends() {
             topmost().navigate({
                 moduleName: "friends/friends-page",
                 clearHistory: true
             });
+        },
+
+        goToMap() {
+            // console.log("Navigate vers Map");
+            topmost().navigate({
+                moduleName: "home/home-page",
+                clearHistory: true
+            });
         }
-        
     });
 
     return viewModel;
